9f4038c90ec8a53acc75c16b6b2ce0449c2ed510
Tai <anhtai3d2y@gmail.com>
2021-12-17 18:20:08 +0700

    17/12/2021

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -5,10 +5,25 @@ class CourseController {
     // [GET] /courses/:slug
     show(req, res, next) {
         Course.findOne({ slug: req.params.slug })
-            .then((course) => {
+            .then((course) =>
                 res.render('courses/show', { course: mongooseToObject(course) })
-            })
+            )
             .catch(next)
+    }
+
+    // [GET] /courses/create
+    create(req, res, next) {
+        res.render('courses/create')
+    }
+
+    // [POST] /courses/store
+    store(req, res, next) {
+        const formData = req.body
+        formData.image = `https://youtu.be/${req.body.videoId}`
+        const course = new Course(formData)
+        course.save()
+
+        res.send('Lưu thành công')
     }
 }
 
--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -4,11 +4,13 @@ const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
 const Course = new Schema({
-    name: { type: String, maxLength: 255 },
-    description: { type: String, maxLength: 600 },
-    image: { type: String, maxLength: 255 },
-    createdAt: { type: Date, default: Date.now },
-    updateAt: { type: Date, default: Date.now },
+    name: { type: String, required: true, },
+    description: { type: String },
+    image: { type: String },
+    videoId: { type: String, required: true, },
+    level: { type: String },
+}, {
+    timestamps: true,
 });
 
 module.exports = mongoose.model('Course', Course);
